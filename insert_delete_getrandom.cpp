13d297c4a68e4d3a4165c715a6bbb50a708d0f35
Josip Seljan <johnnymartian404@gmail.com>
2024-02-12 18:18:13 +0100

    Add faster implementation of the RandomizedSet based on the best solution on leetcode

--- insert_delete_getrandom.cpp
+++ insert_delete_getrandom.cpp
@@ -3,33 +3,29 @@
 #include <climits>
 
 bool RandomizedSet::insert(int val) {
-  auto [_, success] = storage.insert({val, val});
-  if (success) {
-    values.push_back(val);
-  }
+  if (storage.find(val) != storage.end())
+    return false;
+
+  values.push_back(val);
+  auto [_, success] = storage.insert({val, values.size() - 1});
+
   return success;
 }
 
 bool RandomizedSet::remove(int val) {
   auto it = storage.find(val);
-  if (it != storage.end()) {
-    auto next_it = storage.erase(it);
-    return next_it != storage.end();
-  } else {
+  if (it == storage.end())
     return false;
-  }
+
+  values[it->second] = values.back(); // replace the element to be erased with
+  values.pop_back();
+  storage[values[it->second]] = it->second; // update the swapped element index
+
+  storage.erase(it);
+  return true;
 }
 
-int RandomizedSet::get_random() {
-  decltype(storage.find(0)) it{};
-
-  do {
-    auto candidate_i = mt() % values.size();
-    it = storage.find(values[candidate_i]);
-  } while (it == storage.end());
-
-  return it->second;
-}
+int RandomizedSet::get_random() { return values[mt() % values.size()]; }
 
 RandomizedSet make_random_rs(size_t size, int max_val = 1 << 12) {
   static std::mt19937 mt{};
